e3131f0275becc0be38cc6f1c9f5f95c720ad07f
bubibs <bubibs@gmail.com>
2025-12-27 11:31:18 +0100

    Create report.js

---
+++ js/report.js
@@ -0,0 +1,2 @@
+const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwMjZY2BKMAxgcUITrf-BEyb3uXIjToQbTlgGRWjjxdJsse7-azQXzqLiD6IMJS7DKOqw/exec";
+// Inserisci qui tutta la logica di salvataggio report e suggerimenti luogo
